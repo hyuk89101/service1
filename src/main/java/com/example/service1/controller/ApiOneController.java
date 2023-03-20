78ea1814e18c1dea707a0f1b43cddcbc1b27a141
hyuk89101 <kjh89101@gamil.com>
2023-03-20 20:56:18 +0900

    TEST API1

---
+++ src/main/java/com/example/service1/controller/ApiOneController.java
@@ -0,0 +1,19 @@
+package com.example.service1.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class ApiOneController {
+
+    @GetMapping(value = "/reseller/main")
+    public String services() {
+        return String.format("reseller/main API Port : %s", "8011");
+    }
+
+}
