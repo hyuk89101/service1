5492ff255c7bf3713f8f53e91dad9f7014132e54
hyuk89101 <git@khs871004>
2023-03-22 13:47:54 +0900

    CMIT

--- src/main/java/com/example/service1/controller/ApiOneController.java
+++ src/main/java/com/example/service1/controller/ApiOneController.java
@@ -3,6 +3,7 @@ package com.example.service1.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,13 @@ public class ApiOneController {
     @GetMapping(value = "/reseller/main")
     public String services() {
         return String.format("reseller/main API Port : %s", "8011");
+    }
+
+    // message 메서드 추가
+    @GetMapping("/reseller/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in Service1.";
     }
 
 }
